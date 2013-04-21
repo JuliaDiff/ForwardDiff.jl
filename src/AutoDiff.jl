c65a1000fba02a13b9bb5442419c93e0eea5fe2f
theodore <theodore.papamarkou@gmail.com>
2013-04-21 10:34:41 +0100

    Arranged exports and imported DualNumbers

--- src/AutoDiff.jl
+++ src/AutoDiff.jl
@@ -1,59 +1,19 @@
 module AutoDiff
-
-  include("dual.jl")
+  using DualNumbers
+  
+  importall Base
+  
   include("ad_jonas_rauch.jl")
   include("source_transformation.jl")
-
+  
   export
-    # Export Dual type and dual functions
     Dual,
     Dual128,
     Dual64,
     DualPair,
-    real,
-    imag,
     dual,
     dual128,
     dual64,
     isdual,
-    real_valued,
-    integer_valued,
-    isfinite,
-    reim,
-    dual_show,
-    show,
-    showcompact,
-    read,
-    write,
-    ==,
-    isequal,
-    hash,
-    conj,
-    abs,
-    abs2,
-    inv,
-    +,
-    -,
-    *,
-    /,
-    sqrt,
-    cbrt,
-    ^,
-    exp,
-    log,
-    log2,
-    log10,
-    sin,
-    cos,
-    tan,
-    asin,
-    acos,
-    atan,
-    sinh,
-    cosh,
-    tanh,
-    asinh,
-    acosh,
-    atanh
-
+    dual_show
 end # module Autodiff
