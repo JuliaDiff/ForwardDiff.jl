2eff2260fe150af5e5d16535c60a81d858381512
theodore <theodore.papamarkou@gmail.com>
2013-04-28 17:43:02 +0100

    Overloaded division, roots and power operations for FADHessian

--- src/fad_hessian.jl
+++ src/fad_hessian.jl
@@ -117,3 +117,87 @@ end
 
 *{T<:Real, n}(x1::T, x2::FADHessian{T, n}) = FADHessian{T, n}(x1*x2.d, x1*x2.h)
 *{T<:Real, n}(x1::FADHessian{T, n}, x2::T) = FADHessian{T, n}(x2*x1.d, x2*x1.h)
+
+function /{T<:Real, n}(x1::FADHessian{T, n}, x2::FADHessian{T, n})
+  h = Array(T, convert(Int, n*(n+1)/2))
+  k = 1
+  for i in 1:n
+    for j in 1:i
+      h[k] = ((2*x1.d.v*x2.d.g[j]*x2.d.g[i]+x2.d.v*x2.d.v*x1.h[k]
+        -x2.d.v*(x1.d.g[i]*x2.d.g[j]+x1.d.g[j]*x2.d.g[i]+x1.d.v*x2.h[k]))
+        /(x2.d.v*x2.d.v*x2.d.v))
+      k += 1
+    end
+  end
+  FADHessian{T, n}(x1.d/x2.d, h)
+end
+
+function sqrt{T<:Real, n}(x::FADHessian{T, n})
+  h = Array(T, convert(Int, n*(n+1)/2))
+  k = 1
+  for i in 1:n
+    for j in 1:i
+      h[k] = (-x.d.g[i]*x.d.g[j]+2*x.d.v*x.h[k])/(4*(x.d.v^(1.5)))
+      k += 1
+    end
+  end
+  FADHessian{T, n}(sqrt(x.d), h)
+end
+
+function cbrt{T<:Real, n}(x::FADHessian{T, n})
+  h = Array(T, convert(Int, n*(n+1)/2))
+  k = 1
+  for i in 1:n
+    for j in 1:i
+      h[k] = ((-2*x.d.g[i]*x.d.g[j]+3*x.d.v*x.h[k])
+        /(9*cbrt(x.d.v*x.d.v*x.d.v*x.d.v*x.d.v)))
+      k += 1
+    end
+  end
+  FADHessian{T, n}(sqrt(x.d), h)
+end
+
+function ^{T<:Real, n}(x1::FADHessian{T, n}, x2::FADHessian{T, n})
+  h = Array(T, convert(Int, n*(n+1)/2))
+  k = 1
+  for i in 1:n
+    for j in 1:i
+      h[k] = ((x1.d.v^(x2.d.v-2))*(x2.d.v*x2.d.v*x1.d.g[i]*x1.d.g[j]
+      +x2.d.v*(x1.d.g[j]*(-x1.d.g[i]+x1.d.v*log(x1.d.v)*x2.d.g[i])
+      +x1.d.v*(log(x1.d.v)*x1.d.g[i]*x2.d.g[j]+x1.h[k]))
+      +x1.d.v*(x1.d.g[j]*x2.d.g[i]+x2.d.g[j]*(x1.d.g[i]
+      +x1.d.v*log(x1.d.v)*log(x1.d.v)*x2.d.g[i])
+      +x1.d.v*log(x1.d.v)*x2.h[k])))
+      k += 1
+    end
+  end
+  FADHessian{T, n}(x1.d/x2.d, h)
+end
+
+# exp{T<:Real, n}(x::GraDual{T, n}) = GraDual{T, n}(exp(x.v), exp(x.v)*x.g)
+# log{T<:Real, n}(x::GraDual{T, n}) = GraDual{T, n}(log(x.v), x.g/x.v)
+# log2{T<:Real, n}(x::GraDual{T, n}) = log(x)/oftype(T, 0.6931471805599453)
+# log10{T<:Real, n}(x::GraDual{T, n}) = log(x)/oftype(T, 2.302585092994046)
+# 
+# sin{T<:Real, n}(x::GraDual{T, n}) = GraDual{T, n}(sin(x.v), cos(x.v)*x.g)
+# cos{T<:Real, n}(x::GraDual{T, n}) = GraDual{T, n}(cos(x.v), -sin(x.v)*x.g)
+# tan{T<:Real, n}(x::GraDual{T, n}) =
+#   GraDual{T, n}(tan(x.v), square(sec(x.v))*x.g)
+# 
+# asin{T<:Real, n}(x::GraDual{T, n}) =
+#   GraDual{T, n}(asin(x.v), x.g/sqrt(1-square(x.v)))
+# acos{T<:Real, n}(x::GraDual{T, n}) =
+#   GraDual{T, n}(acos(x.v), -x.g/sqrt(1-square(x.v)))
+# atan{T<:Real, n}(x::GraDual{T, n}) =
+#   GraDual{T, n}(atan(x.v), x.g/(1+square(x.v)))
+# 
+# sinh{T<:Real, n}(x::GraDual{T, n}) = GraDual{T, n}(sinh(x.v), cosh(x.v)*x.g)
+# cosh{T<:Real, n}(x::GraDual{T, n}) = GraDual{T, n}(cosh(x.v), sinh(x.v)*x.g)
+# tanh{T<:Real, n}(x::GraDual{T, n}) =
+#   GraDual{T, n}(tanh(x.v), 1-square(tanh(x.v))*x.g)
+# 
+# asinh{T<:Real, n}(x::GraDual{T, n}) =
+#   GraDual{T, n}(asinh(x.v), x.g/sqrt(x.v*x.v+1))
+# acosh{T<:Real, n}(x::GraDual{T, n}) =
+#   GraDual{T, n}(acosh(x.v), x.g/sqrt(x.v*x.v-1))
+# atanh{T<:Real, n}(x::GraDual{T, n}) = GraDual{T, n}(atanh(x.v), x.g/(1-x.v*x.v))
--- test/fad_hessian_ad.jl
+++ test/fad_hessian_ad.jl
@@ -1,7 +1,8 @@
 using AutoDiff
 
 args = FADHessian([1., 2., 3.])
-f(x, y, z) = x*x*x+y*y*y+z*z*z+x*y*y+y*z*z+x*z*z
+#f(x, y, z) = x*x*x+y*y*y+z*z*z+x*y*y+y*z*z+x*z*z
+f(x, y, z) = x^3+y^3+z^3+x*y^2+y*z^2+x*z^2
 w = f(args...)
 
 println("f(x, y, z) = x^3+y^3+z^3+x*y^2+y*z^2+x*z^2\n")
