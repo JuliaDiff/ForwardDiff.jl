8fb30e19d63eccd3efdcfd311b7e69bb670e5bb2
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-03-06 23:21:40 +0000

    Updated GraDual test once again (higher powers so as to use it with tensors too)

--- test/GraDual.jl
+++ test/GraDual.jl
@@ -5,18 +5,20 @@ using Base.Test
 
 # Testing addition, subtraction, scalar multiplication, multiplication and integer powers
 
-f(x, y, z) = x^3-y^3+z^3+x^2*y-5*x*z^2+x*y*z
-dfdx(x, y, z) = 3*x^2+2*x*y-5*z^2+y*z
-dfdy(x, y, z) = -3*y^2+x^2+x*z
-dfdz(x, y, z) = 3*z^2-10*x*z+x*y
+f(x, y, z) = x^6-y^5+z^2+x^4*y*z-3*x*y^3*z^2+2*x^2*y^3-4*y^2*z+y*z^2-2*x*y*z
+dfdx(x, y, z) = 6*x^5+4*x^3*y*z-3*y^3*z^2+4*x*y^3-2*y*z
+dfdy(x, y, z) = -5*y^4+x^4*z-9*x*y^2*z^2+6*x^2*y^2-8*y*z+z^2-2*x*z
+dfdz(x, y, z) = 2*z+x^4*y-6*x*y^3*z-4*y^2+2*y*z-2*x*y
+gradf(x, y, z) = [dfdx(x, y, z), dfdy(x, y, z), dfdz(x, y, z)]
 
 args = [2., 5., -1.]
 output = f(GraDual(args)...)
 
 @test_approx_eq value(output) f(args...)
-@test_approx_eq grad(output) [dfdx(args...), dfdy(args...), dfdz(args...)]
+@test_approx_eq grad(output) gradf(args...)
 
 # Testing the API
+
 g = forwarddiff_jacobian(f, Float64, n=3, fadtype=:typed)
 output = g(args)
-@test_approx_eq output [dfdx(args...), dfdy(args...), dfdz(args...)]
+@test_approx_eq output gradf(args...)
