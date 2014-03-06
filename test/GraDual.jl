2d9cc0a76f519c5867c0e2e683104f6c27c1bfc6
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-03-06 18:47:54 +0000

    Updated GraDual test (more generic so as to test with varying values of arguments)

--- test/GraDual.jl
+++ test/GraDual.jl
@@ -1,26 +1,22 @@
-## Calculation of f(2, 5), df(2, 5)/dx and df(2, 5)/dy for function 
-## f:R^2->R^2 given by f(x, y) = [3*x*y, y^2]
-
 using ForwardDiff
 using Base.Test
 
-args = GraDual([2., 5.])
+### Testing functions f:R^n->R
 
-f(x, y) = [3*x*y, y^2]
-#f(x) = [3*x[1]*x[2], x[2]^2] # works too
+# Testing addition, subtraction, scalar multiplication, multiplication and integer powers
 
-args = GraDual([2., 5.])
+f(x, y, z) = x^3-y^3+z^3+x^2*y-5*x*z^2+x*y*z
+dfdx(x, y, z) = 3*x^2+2*x*y-5*z^2+y*z
+dfdy(x, y, z) = -3*y^2+x^2+x*z
+dfdz(x, y, z) = 3*z^2-10*x*z+x*y
 
-y = f(args...)
-#println("f(x, y) = [3*x*y, y^2]\n")
+args = [2., 5., -1.]
+output = f(GraDual(args)...)
 
-@test value(y) == [30, 25]
-#println("f(2, 5) = \n", value(y))
-
-@test jacobian(y) == [15 6; 0 10]
-#println("Jacobian J(f):\n", jacobian(y))
+@test_approx_eq value(output) f(args...)
+@test_approx_eq grad(output) [dfdx(args...), dfdy(args...), dfdz(args...)]
 
 # Testing the API
-g = forwarddiff_jacobian(f, Float64, n=2, fadtype=:typed)
-out = g([2., 5.])
-@test out == [15 6; 0 10]
+g = forwarddiff_jacobian(f, Float64, n=3, fadtype=:typed)
+output = g(args)
+@test_approx_eq output [dfdx(args...), dfdy(args...), dfdz(args...)]
