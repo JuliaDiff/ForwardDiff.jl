7e1551aff8e725d0519745ec522dcb54644868bf
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-03-13 12:00:08 +0000

    Added FADTensor log10

--- src/typed_fad/FADTensor.jl
+++ src/typed_fad/FADTensor.jl
@@ -233,7 +233,29 @@ function log2{T<:Real, n}(x::FADTensor{T, n})
            +x.h.d.g[i]*x.h.h[m]
            +x.h.d.g[j]*x.h.h[l]
           ))/x.h.d.v
-          +x.t[q])/(x.h.d.v*log(2))
+          +x.t[q])/(x.h.d.v*oftype(T, 0.6931471805599453))
+        )
+        q += 1
+      end
+    end
+  end
+  FADTensor{T, n}(exp(x.h), t)
+end
+
+function log10{T<:Real, n}(x::FADTensor{T, n})
+  t = Array(T, convert(Int, n*n*(n+1)/2))
+  local l, m, r
+  q = 1
+  for a in 1:n
+    for i in 1:n
+      for j in 1:i
+        l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
+        t[q] = (((2*x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j]/x.h.d.v
+          -(x.h.d.g[a]*x.h.h[r]
+           +x.h.d.g[i]*x.h.h[m]
+           +x.h.d.g[j]*x.h.h[l]
+          ))/x.h.d.v
+          +x.t[q])/(x.h.d.v*oftype(T, 2.302585092994046))
         )
         q += 1
       end
