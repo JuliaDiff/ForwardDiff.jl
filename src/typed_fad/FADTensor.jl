55033b4b36ecb8a416a35f450c78228bd92b2147
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-03-24 06:02:05 +0000

    Refactored FADTensors (nice speedup for tensors!)

--- src/typed_fad/FADTensor.jl
+++ src/typed_fad/FADTensor.jl
@@ -5,7 +5,7 @@ end
 
 FADTensor{T<:Real, n}(h::FADHessian{T, n}, t::Vector{T}) = FADTensor{T, length(h.d.g)}(h, t)
 
-FADTensor{T<:Real, n}(h::FADHessian{T, n}) = FADTensor{T, length(h.d.g)}(h, convert(Int, n*n*(n+1)/2))
+FADTensor{T<:Real, n}(h::FADHessian{T, n}) = FADTensor{T, length(h.d.g)}(h, convert(Int, n*(n+1)*(n+2)/6))
 
 function FADTensor{T<:Real}(v::Vector{T})
   n = length(v)
@@ -14,13 +14,13 @@ function FADTensor{T<:Real}(v::Vector{T})
     g = zeros(T, n)
     g[i] = one(T)
     Tensor[i] = FADTensor(FADHessian(GraDual{T, n}(v[i], g), zeros(T, convert(Int, n*(n+1)/2))), 
-      zeros(T, convert(Int, n*n*(n+1)/2)))
+      zeros(T, convert(Int, n*(n+1)*(n+2)/6)))
   end
   return Tensor
 end
 
-zero{T, n}(::Type{FADTensor{T, n}}) = FADTensor(zero(FADHessian{T, n}), zeros(T, convert(Int, n*n*(n+1)/2)))
-one{T, n}(::Type{FADTensor{T, n}}) = FADTensor(one(FADHessian{T, n}), zeros(T, convert(Int, n*n*(n+1)/2)))
+zero{T, n}(::Type{FADTensor{T, n}}) = FADTensor(zero(FADHessian{T, n}), zeros(T, convert(Int, n*(n+1)*(n+2)/6)))
+one{T, n}(::Type{FADTensor{T, n}}) = FADTensor(one(FADHessian{T, n}), zeros(T, convert(Int, n*(n+1)*(n+2)/6)))
 
 value(x::FADTensor) = value(x.h)
 value{T<:Real, n}(X::Vector{FADTensor{T, n}}) = [value(x) for x in X]
@@ -38,10 +38,22 @@ function tensor{T<:Real, n}(x::FADTensor{T, n})
   k = 1
   
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         y[i, j, a] = x.t[k]
         k += 1
+      end
+    end
+
+    for i in 1:(a-1)
+      for j in 1:i
+        y[i, j, a] = y[a, i, j]
+      end
+    end
+
+    for i in a:n
+      for j in 1:(a-1)
+        y[i, j, a] = y[a, i, j]
       end
     end
 
@@ -57,15 +69,15 @@ end
 
 convert{T<:Real, n}(::Type{FADTensor{T, n}}, x::FADTensor{T, n}) = x
 convert{T<:Real, n}(::Type{FADTensor{T, n}}, x::T) =
-  FADTensor(FADHessian{T, n}(x, zeros(T, convert(Int, n*(n+1)/2))), zeros(T, convert(Int, n*n*(n+1)/2)))
+  FADTensor(FADHessian{T, n}(x, zeros(T, convert(Int, n*(n+1)/2))), zeros(T, convert(Int, n*(n+1)*(n+2)/6)))
 convert{T<:Real, S<:Real, n}(::Type{FADTensor{T, n}}, x::S) = 
-  FADTensor(FADHessian{T, n}(convert(T, x), zeros(T, convert(Int, n*(n+1)/2))), zeros(T, convert(Int, n*n*(n+1)/2)))
+  FADTensor(FADHessian{T, n}(convert(T, x), zeros(T, convert(Int, n*(n+1)/2))), zeros(T, convert(Int, n*(n+1)*(n+2)/6)))
 convert{T<:Real, S<:Real, n}(::Type{FADTensor{T, n}}, x::FADTensor{S, n}) =
   FADTensor(FADHessian{T, n}(GraDual{T, n}(convert(T, x.h.d.v), convert(Vector{T}, x.h.d.g)),
   convert(Vector{T}, x.h.h)), convert(Vector{T}, x.t))
 convert{T<:Real, S<:Real, n}(::Type{T}, x::FADTensor{S, n}) =
   ((x.h.d.g == zeros(S, n) && x.h.h == zeros(S, convert(Int, n*(n+1)/2)) && 
-    x.h.t == zeros(S, convert(Int, n*n*(n+1)/2))) ? convert(T, x.h.d.v) : throw(InexactError()))
+    x.h.t == zeros(S, convert(Int, n*(n+1)*(n+2)/6))) ? convert(T, x.h.d.v) : throw(InexactError()))
 
 promote_rule{T<:Real, n}(::Type{FADTensor{T, n}}, ::Type{T}) = FADTensor{T, n}
 promote_rule{T<:Real, S<:Real, n}(::Type{FADTensor{T, n}}, ::Type{S}) = FADTensor{promote_type(T, S), n}
@@ -74,9 +86,9 @@ promote_rule{T<:Real, S<:Real, n}(::Type{FADTensor{T, n}}, ::Type{FADTensor{S, n
 isfadtensor(x::FADTensor) = true
 isfadtensor(x::Number) = false
 
-isconstant{T<:Real, n}(x::FADTensor{T, n}) = (isconstant(x.h) && x.t == zeros(T, convert(Int, n*n*(n+1)/2)))
+isconstant{T<:Real, n}(x::FADTensor{T, n}) = (isconstant(x.h) && x.t == zeros(T, convert(Int, n*(n+1)*(n+2)/6)))
 iszero{T<:Real, n}(x::FADTensor{T, n}) = isconstant(x) && (x.h.d.v == zero(T))
-isfinite{T<:Real, n}(x::FADTensor{T, n}) = (isfinite(x.h) && x.t == ones(T, convert(Int, n*n*(n+1)/2)))
+isfinite{T<:Real, n}(x::FADTensor{T, n}) = (isfinite(x.h) && x.t == ones(T, convert(Int, n*(n+1)*(n+2)/6)))
 
 =={T<:Real, n}(x1::FADTensor{T, n}, x2::FADTensor{T, n}) = ((x1.h == x2.h) && (x1.t == x2.t))
   
@@ -98,12 +110,12 @@ end
 -{T<:Real, n}(x1::FADTensor{T, n}, x2::FADTensor{T, n}) = FADTensor{T, n}(x1.h-x2.h, x1.t-x2.t)
 
 function *{T<:Real, n}(x1::FADTensor{T, n}, x2::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] =
          (x2.h.d.g[a]*x1.h.h[r]+x1.h.d.g[a]*x2.h.h[r]
@@ -123,12 +135,12 @@ end
 *{T<:Real, n}(x1::FADTensor{T, n}, x2::T) = FADTensor{T, n}(x2*x1.d, x2*x1.h)
 
 function /{T<:Real, n}(x1::FADTensor{T, n}, x2::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = ((x1.t[q]
           +(-(x1.h.d.g[a]*x2.h.h[r]
@@ -153,12 +165,12 @@ function /{T<:Real, n}(x1::FADTensor{T, n}, x2::FADTensor{T, n})
 end
 
 function sqrt{T<:Real, n}(x::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (((0.375*x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j]/x.h.d.v
           -0.25*(x.h.d.g[a]*x.h.h[r]
@@ -175,12 +187,12 @@ function sqrt{T<:Real, n}(x::FADTensor{T, n})
 end
 
 function cbrt{T<:Real, n}(x::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (((10*x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j]/(3*x.h.d.v)
           -2*(x.h.d.g[a]*x.h.h[r]
@@ -200,12 +212,12 @@ end
 ^{T1<:Real, T2<:Rational, n}(x::FADTensor{T1, n}, p::T2) = x^convert(FloatingPoint, p)
 
 function ^{T1<:Real, T2<:Real, n}(x::FADTensor{T1, n}, p::T2)
-  t = Array(T1, convert(Int, n*n*(n+1)/2))
+  t = Array(T1, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (p*(
           (p-1)*x.h.d.v^(p-3)*(
@@ -221,12 +233,12 @@ function ^{T1<:Real, T2<:Real, n}(x::FADTensor{T1, n}, p::T2)
 end
 
 function exp{T<:Real, n}(x::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (exp(x.h.d.v)*(
           x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j]
@@ -243,12 +255,12 @@ function exp{T<:Real, n}(x::FADTensor{T, n})
 end
 
 function log{T<:Real, n}(x::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (((2*x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j]/x.h.d.v
           -(x.h.d.g[a]*x.h.h[r]
@@ -265,12 +277,12 @@ function log{T<:Real, n}(x::FADTensor{T, n})
 end
 
 function log2{T<:Real, n}(x::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (((2*x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j]/x.h.d.v
           -(x.h.d.g[a]*x.h.h[r]
@@ -287,12 +299,12 @@ function log2{T<:Real, n}(x::FADTensor{T, n})
 end
 
 function log10{T<:Real, n}(x::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (((2*x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j]/x.h.d.v
           -(x.h.d.g[a]*x.h.h[r]
@@ -309,12 +321,12 @@ function log10{T<:Real, n}(x::FADTensor{T, n})
 end
 
 function sin{T<:Real, n}(x::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (
           cos(x.h.d.v)*(x.t[q]-x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j])
@@ -328,12 +340,12 @@ function sin{T<:Real, n}(x::FADTensor{T, n})
 end
 
 function cos{T<:Real, n}(x::FADTensor{T, n})
-  t = Array(T, convert(Int, n*n*(n+1)/2))
+  t = Array(T, convert(Int, n*(n+1)*(n+2)/6))
   local l, m, r
   q = 1
   for a in 1:n
-    for i in 1:n
-      for j in 1:i
+    for i in a:n
+      for j in a:i
         l, m, r = t2h(a, i), t2h(a, j), t2h(i, j)
         t[q] = (
           sin(x.h.d.v)*(x.h.d.g[a]*x.h.d.g[i]*x.h.d.g[j]-x.t[q])
