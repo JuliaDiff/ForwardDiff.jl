926f50d763568a650a2badb8668bf4462576c54a
Jarrett Revels <jarrettrevels@gmail.com>
2015-08-05 15:15:33 -0400

    slightly better test for derivatives. Involves bivariate functions and tests rank 3 tensor output

--- test/test_derivatives.jl
+++ test/test_derivatives.jl
@@ -6,6 +6,8 @@ using Calculus
 # Test taking derivatives #
 ###########################
 N = 4
+M = 2N
+L = 3N
 
 function deriv_test_x(fsym)
     randrange = 0.01:.01:.99
@@ -18,7 +20,7 @@ function deriv_test_x(fsym)
     return rand(randrange)
 end
 
-function test_approx_deriv(a::Vector, b::Vector)
+function test_approx_deriv(a, b)
     @assert length(a) == length(b)
     for i in eachindex(a)
         @test_approx_eq a[i] b[i]
@@ -26,22 +28,23 @@ function test_approx_deriv(a::Vector, b::Vector)
 end
 
 for fsym in ForwardDiff.fad_supported_univar_funcs
-    deriv = Calculus.differentiate(:($(fsym)(x)))
+    func_expr = :($(fsym)(x) + 4^$(fsym)(x) - x * $(fsym)(x))
+    deriv = Calculus.differentiate(func_expr)
     try 
         @eval begin 
             x = deriv_test_x($fsym)
-            f_num = $fsym
+            testdf = x -> $func_expr
             result = $deriv
             
-            @test_approx_eq result ForwardDiff.derivative(f_num, x)
-            @test_approx_eq result ForwardDiff.derivative(f_num)(x)
+            @test_approx_eq result ForwardDiff.derivative(testdf, x)
+            @test_approx_eq result ForwardDiff.derivative(testdf)(x)
 
-            f_vec = t -> [f_num(t) for i=1:N]
-            result_vec = [result for i=1:N]
+            testdf_arr = t -> [testdf(t) for i in 1:N, j in 1:M, k in 1:L]
+            result_arr = [result for i in 1:N, j in 1:M, k in 1:L]
 
-            test_approx_deriv(result_vec, ForwardDiff.derivative(f_vec, x))
-            test_approx_deriv(result_vec, ForwardDiff.derivative(f_vec)(x))
-            test_approx_deriv(result_vec, ForwardDiff.derivative!(f_vec, x, Vector{Float64}(N)))
+            test_approx_deriv(result_arr, ForwardDiff.derivative(testdf_arr, x))
+            test_approx_deriv(result_arr, ForwardDiff.derivative(testdf_arr)(x))
+            test_approx_deriv(result_arr, ForwardDiff.derivative!(testdf_arr, x, Array(Float64, N, M, L)))
         end
     catch err
         error("Failure when testing derivative of $fsym: $err")
