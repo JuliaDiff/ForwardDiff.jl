40306b752467c630229228b069efbc6b0ef451f5
Jarrett Revels <jarrettrevels@gmail.com>
2015-08-10 13:09:24 -0400

    rename test files to better reflect what they cover

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using ForwardDiff
 
-include("test_gradnum.jl")
-include("test_hessnum.jl")
-include("test_tensnum.jl")
+include("test_gradients.jl")
+include("test_hessians.jl")
+include("test_tensors.jl")
 include("test_derivatives.jl")
 include("test_jacobians.jl")
