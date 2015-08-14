c2ccc767b59c3e7a10b4450df1d57c7a35315b21
Jarrett Revels <jarrettrevels@gmail.com>
2015-08-14 10:02:13 -0400

    rm noisey comments

--- benchmarks/benchmarks.jl
+++ benchmarks/benchmarks.jl
@@ -22,18 +22,11 @@ end
 #############################
 # Usage:
 #
-# # The values of the Dict are Arrays of time values where indices correspond to length(x)
-# julia> t = bench_fad(ackley, 10:10:100, 4) # benchmark ackley where length(x) = 10:10:100, taking the minimum of 4 trials
-# Dict{Symbol,Array{Float64,1}} with 3 entries:
-#   :gtimes => [4.152e-6,1.8057e-5,2.0612e-5,3.546e-5,3.9163e-5,9.3705e-5,5.6879e-5,6.3378e-5,9.8352e-5,0.000106597]
-#   :htimes => [2.5515e-5,7.4746e-5,0.000164618,0.000345889,0.000773864,0.00110334,0.002396697,0.003085223,0.004590107,…
-#   :ftimes => [7.83e-7,9.26e-7,1.11e-6,1.162e-6,1.324e-6,1.504e-6,1.569e-6,1.766e-6,2.002e-6,2.056e-6]
+# benchmark ackley where length(x) = 10:10:100, taking the minimum of 4 trials:
+# bench_fad(ackley, 10:10:100, 4)
 #
-# julia> t = bench_fad(ackley, 400, 4)
-# Dict{Symbol,Array{Float64,1}} with 3 entries:
-#   :gtimes => [0.00109457]
-#   :htimes => [0.725081113]
-#   :ftimes => [6.836e-6]
+# benchmark ackley where len(x) = 400, taking the minimum of 8 trials:
+# bench_fad(ackley, 400, 4)
 
 function bench_fad(f, range, repeat=3)
     g = ForwardDiff.gradient(f)
