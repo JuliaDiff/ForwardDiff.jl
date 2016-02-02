de097a67715689326825f6e4b417ed4b5d7eddd3
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2016-02-02 09:54:41 +0100

    fix trailing whitespaces

--- src/api/api.jl
+++ src/api/api.jl
@@ -1,7 +1,7 @@
-# Note for the files in this folder: 
-# Following convention, methods whose names are prefixed 
-# with an underscore are unsafe to use outside of a strictly 
-# controlled context - such methods assume that all 
+# Note for the files in this folder:
+# Following convention, methods whose names are prefixed
+# with an underscore are unsafe to use outside of a strictly
+# controlled context - such methods assume that all
 # boundary-checking is done by the caller.
 
 const tuple_usage_threshold = 10
@@ -9,7 +9,7 @@ const default_chunk_size = 0
 
 abstract AllResults
 
-function check_chunk_size(xlen::Int, chunk_size::Int)        
+function check_chunk_size(xlen::Int, chunk_size::Int)
     if chunk_size != default_chunk_size
         @assert chunk_size > 0 "Invalid chunk_size: $chunk_size. chunk_size cannot be negative."
         @assert xlen % chunk_size == 0 "Length of input vector is indivisible by chunk size (length(x) = $xlen, chunk size = $chunk_size)"
