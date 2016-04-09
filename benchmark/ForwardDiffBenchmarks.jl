5da45916f639fc4834ecee15f6998fac754c1c36
Jarrett Revels <jarrettrevels@gmail.com>
2016-04-09 11:23:12 -0400

    update benchmarks w.r.t. recent BenchmarkTools changes

--- benchmark/ForwardDiffBenchmarks.jl
+++ benchmark/ForwardDiffBenchmarks.jl
@@ -7,13 +7,13 @@ include(joinpath(Pkg.dir("ForwardDiff"), "test", "TestFuncs.jl"))
 
 name(f) = last(split(string(f), '.'))
 
-const GROUPS = BenchmarkTools.GroupCollection()
+const SUITE = BenchmarkTools.BenchmarkGroup()
 
 xs = map(n -> rand(MersenneTwister(1), n), (12, 120, 1200, 12000))
 chunks = 1:10
 
-val = addgroup!(GROUPS, "value")
-grad = addgroup!(GROUPS, "gradient")
+val = newgroup!(SUITE, "value")
+grad = newgroup!(SUITE, "gradient")
 
 for f in TestFuncs.VECTOR_TO_NUMBER_FUNCS
     for x in xs
