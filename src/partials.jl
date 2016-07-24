2f712dc1da9c801b537c31ea5cc3400987043124
Jarrett Revels <jarrettrevels@gmail.com>
2016-07-24 14:13:34 -0400

    replace operator macro with compat, bump required Compat version (#134)

--- src/partials.jl
+++ src/partials.jl
@@ -39,7 +39,7 @@ Base.done(partials::Partials, i) = done(partials.values, i)
 @inline Base.rand{N,T}(rng::AbstractRNG, ::Type{Partials{N,T}}) = Partials{N,T}(rand_tuple(rng, NTuple{N,T}))
 
 Base.isequal{N}(a::Partials{N}, b::Partials{N}) = isequal(a.values, b.values)
-@operator(Base.:(==)){N}(a::Partials{N}, b::Partials{N}) = a.values == b.values
+@compat(Base.:(==)){N}(a::Partials{N}, b::Partials{N}) = a.values == b.values
 
 const PARTIALS_HASH = hash(Partials)
 
@@ -69,12 +69,12 @@ Base.convert{N,T}(::Type{Partials{N,T}}, partials::Partials{N,T}) = partials
 # Arithmetic Functions #
 ########################
 
-@inline @operator(Base.:+){N}(a::Partials{N}, b::Partials{N}) = Partials(add_tuples(a.values, b.values))
-@inline @operator(Base.:-){N}(a::Partials{N}, b::Partials{N}) = Partials(sub_tuples(a.values, b.values))
-@inline @operator(Base.:-)(partials::Partials) = Partials(minus_tuple(partials.values))
-@inline @operator(Base.:*)(partials::Partials, x::Real) = Partials(scale_tuple(partials.values, x))
-@inline @operator(Base.:*)(x::Real, partials::Partials) = partials*x
-@inline @operator(Base.:/)(partials::Partials, x::Real) = Partials(div_tuple_by_scalar(partials.values, x))
+@inline @compat(Base.:+){N}(a::Partials{N}, b::Partials{N}) = Partials(add_tuples(a.values, b.values))
+@inline @compat(Base.:-){N}(a::Partials{N}, b::Partials{N}) = Partials(sub_tuples(a.values, b.values))
+@inline @compat(Base.:-)(partials::Partials) = Partials(minus_tuple(partials.values))
+@inline @compat(Base.:*)(partials::Partials, x::Real) = Partials(scale_tuple(partials.values, x))
+@inline @compat(Base.:*)(x::Real, partials::Partials) = partials*x
+@inline @compat(Base.:/)(partials::Partials, x::Real) = Partials(div_tuple_by_scalar(partials.values, x))
 
 @inline function _mul_partials{N}(a::Partials{N}, b::Partials{N}, afactor, bfactor)
     return Partials(mul_tuples(a.values, b.values, afactor, bfactor))
@@ -89,12 +89,12 @@ end
 # edge cases where N == 0 #
 #-------------------------#
 
-@inline @operator(Base.:+){A,B}(a::Partials{0,A}, b::Partials{0,B}) = Partials{0,promote_type(A,B)}(tuple())
-@inline @operator(Base.:-){A,B}(a::Partials{0,A}, b::Partials{0,B}) = Partials{0,promote_type(A,B)}(tuple())
-@inline @operator(Base.:-){T}(partials::Partials{0,T}) = partials
-@inline @operator(Base.:*){T}(partials::Partials{0,T}, x::Real) = Partials{0,promote_type(T,typeof(x))}(tuple())
-@inline @operator(Base.:*){T}(x::Real, partials::Partials{0,T}) = Partials{0,promote_type(T,typeof(x))}(tuple())
-@inline @operator(Base.:/){T}(partials::Partials{0,T}, x::Real) = Partials{0,promote_type(T,typeof(x))}(tuple())
+@inline @compat(Base.:+){A,B}(a::Partials{0,A}, b::Partials{0,B}) = Partials{0,promote_type(A,B)}(tuple())
+@inline @compat(Base.:-){A,B}(a::Partials{0,A}, b::Partials{0,B}) = Partials{0,promote_type(A,B)}(tuple())
+@inline @compat(Base.:-){T}(partials::Partials{0,T}) = partials
+@inline @compat(Base.:*){T}(partials::Partials{0,T}, x::Real) = Partials{0,promote_type(T,typeof(x))}(tuple())
+@inline @compat(Base.:*){T}(x::Real, partials::Partials{0,T}) = Partials{0,promote_type(T,typeof(x))}(tuple())
+@inline @compat(Base.:/){T}(partials::Partials{0,T}, x::Real) = Partials{0,promote_type(T,typeof(x))}(tuple())
 
 @inline _mul_partials{A,B}(a::Partials{0,A}, b::Partials{0,B}, afactor, bfactor) = Partials{0,promote_type(A,B)}(tuple())
 @inline _div_partials{A,B}(a::Partials{0,A}, b::Partials{0,B}, afactor, bfactor) = Partials{0,promote_type(A,B)}(tuple())
