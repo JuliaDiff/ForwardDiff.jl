e7d062f381634c7b671b136ec40712f5cdb71907
Jarrett Revels <jarrettrevels@gmail.com>
2017-02-14 12:18:13 -0500

    drop compat macro usage for Base.:symbol only needed for v0.4

--- src/partials.jl
+++ src/partials.jl
@@ -42,7 +42,7 @@ Base.linearindexing(::Partials) = Base.LinearFast()
 @inline Base.rand{N,T}(rng::AbstractRNG, ::Type{Partials{N,T}}) = Partials{N,T}(rand_tuple(rng, NTuple{N,T}))
 
 Base.isequal{N}(a::Partials{N}, b::Partials{N}) = isequal(a.values, b.values)
-@compat(Base.:(==)){N}(a::Partials{N}, b::Partials{N}) = a.values == b.values
+Base.:(==){N}(a::Partials{N}, b::Partials{N}) = a.values == b.values
 
 const PARTIALS_HASH = hash(Partials)
 
@@ -72,10 +72,10 @@ Base.convert{N,T}(::Type{Partials{N,T}}, partials::Partials{N,T}) = partials
 # Arithmetic Functions #
 ########################
 
-@inline @compat(Base.:+){N}(a::Partials{N}, b::Partials{N}) = Partials(add_tuples(a.values, b.values))
-@inline @compat(Base.:-){N}(a::Partials{N}, b::Partials{N}) = Partials(sub_tuples(a.values, b.values))
-@inline @compat(Base.:-)(partials::Partials) = Partials(minus_tuple(partials.values))
-@inline @compat(Base.:*)(x::Real, partials::Partials) = partials*x
+@inline Base.:+{N}(a::Partials{N}, b::Partials{N}) = Partials(add_tuples(a.values, b.values))
+@inline Base.:-{N}(a::Partials{N}, b::Partials{N}) = Partials(sub_tuples(a.values, b.values))
+@inline Base.:-(partials::Partials) = Partials(minus_tuple(partials.values))
+@inline Base.:*(x::Real, partials::Partials) = partials*x
 
 @inline function _div_partials(a::Partials, b::Partials, aval, bval)
     return _mul_partials(a, b, inv(bval), -(aval / (bval*bval)))
@@ -85,12 +85,12 @@ end
 #----------------------#
 
 if NANSAFE_MODE_ENABLED
-    @inline function @compat(Base.:*)(partials::Partials, x::Real)
+    @inline function Base.:*(partials::Partials, x::Real)
         x = ifelse(!isfinite(x) && iszero(partials), one(x), x)
         return Partials(scale_tuple(partials.values, x))
     end
 
-    @inline function @compat(Base.:/)(partials::Partials, x::Real)
+    @inline function Base.:/(partials::Partials, x::Real)
         x = ifelse(x == zero(x) && iszero(partials), one(x), x)
         return Partials(div_tuple_by_scalar(partials.values, x))
     end
@@ -101,11 +101,11 @@ if NANSAFE_MODE_ENABLED
         return Partials(mul_tuples(a.values, b.values, x_a, x_b))
     end
 else
-    @inline function @compat(Base.:*)(partials::Partials, x::Real)
+    @inline function Base.:*(partials::Partials, x::Real)
         return Partials(scale_tuple(partials.values, x))
     end
 
-    @inline function @compat(Base.:/)(partials::Partials, x::Real)
+    @inline function Base.:/(partials::Partials, x::Real)
         return Partials(div_tuple_by_scalar(partials.values, x))
     end
 
@@ -117,12 +117,12 @@ end
 # edge cases where N == 0 #
 #-------------------------#
 
-@inline @compat(Base.:+){A,B}(a::Partials{0,A}, b::Partials{0,B}) = Partials{0,promote_type(A,B)}(tuple())
-@inline @compat(Base.:-){A,B}(a::Partials{0,A}, b::Partials{0,B}) = Partials{0,promote_type(A,B)}(tuple())
-@inline @compat(Base.:-){T}(partials::Partials{0,T}) = partials
-@inline @compat(Base.:*){T}(partials::Partials{0,T}, x::Real) = Partials{0,promote_type(T,typeof(x))}(tuple())
-@inline @compat(Base.:*){T}(x::Real, partials::Partials{0,T}) = Partials{0,promote_type(T,typeof(x))}(tuple())
-@inline @compat(Base.:/){T}(partials::Partials{0,T}, x::Real) = Partials{0,promote_type(T,typeof(x))}(tuple())
+@inline Base.:+{A,B}(a::Partials{0,A}, b::Partials{0,B}) = Partials{0,promote_type(A,B)}(tuple())
+@inline Base.:-{A,B}(a::Partials{0,A}, b::Partials{0,B}) = Partials{0,promote_type(A,B)}(tuple())
+@inline Base.:-{T}(partials::Partials{0,T}) = partials
+@inline Base.:*{T}(partials::Partials{0,T}, x::Real) = Partials{0,promote_type(T,typeof(x))}(tuple())
+@inline Base.:*{T}(x::Real, partials::Partials{0,T}) = Partials{0,promote_type(T,typeof(x))}(tuple())
+@inline Base.:/{T}(partials::Partials{0,T}, x::Real) = Partials{0,promote_type(T,typeof(x))}(tuple())
 
 @inline _mul_partials{A,B}(a::Partials{0,A}, b::Partials{0,B}, afactor, bfactor) = Partials{0,promote_type(A,B)}(tuple())
 @inline _div_partials{A,B}(a::Partials{0,A}, b::Partials{0,B}, afactor, bfactor) = Partials{0,promote_type(A,B)}(tuple())
