7d3247e1b5a89a1c7ff760c673d725b45a9e1b89
Sheehan Olver <solver@mac.com>
2021-06-22 15:05:54 +0100

    Update dual.jl

--- src/dual.jl
+++ src/dual.jl
@@ -684,7 +684,7 @@ function Base.show(io::IO, d::Dual{T,V,N}) where {T,V,N}
 end
 
 for op in (:(Base.typemin), :(Base.typemax), :(Base.floatmin), :(Base.floatmax))
-    @eval function Base.typemin(::Type{ForwardDiff.Dual{T,V,N}}) where {T,V,N}
+    @eval function $op(::Type{ForwardDiff.Dual{T,V,N}}) where {T,V,N}
         ForwardDiff.Dual{T,V,N}($op(V))
     end
 end
