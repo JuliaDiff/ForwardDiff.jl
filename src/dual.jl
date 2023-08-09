81c38910c5e43068b4bd3ff620defd3532c7b554
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2023-08-09 03:22:45 -0400

    Update src/dual.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/dual.jl
+++ src/dual.jl
@@ -360,7 +360,7 @@ end
 @inline Base.one(d::Dual) = one(typeof(d))
 @inline Base.one(::Type{Dual{T,V,N}}) where {T,V,N} = Dual{T}(one(V), zero(Partials{N,V}))
 
-@inline Base.oneunit(d::Dual) = oneunit(typeof(d))
+@inline Base.oneunit(d::Dual{T}) where {T} = Dual{T}(oneunit(primal(d)), zero(partials(d)))
 @inline Base.oneunit(::Type{Dual{T,V,N}}) where {T,V,N} = Dual{T}(oneunit(V), zero(Partials{N,V}))
 
 @inline function Base.Int(d::Dual)
