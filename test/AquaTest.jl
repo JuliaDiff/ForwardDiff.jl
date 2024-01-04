abe0b36ec1836a07fb48223bc5f115a7bae68a3a
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-01-04 09:50:17 -0500

    Update

--- test/AquaTest.jl
+++ test/AquaTest.jl
@@ -2,12 +2,14 @@ using Test
 using ForwardDiff
 using Aqua
 
-@testset "Aqua tests (performance)" begin
+@testset "Aqua tests - unbound_args" begin
     # This tests that we don't accidentally run into
     # https://github.com/JuliaLang/julia/issues/29393
     ua = Aqua.detect_unbound_args_recursively(ForwardDiff)
     @test length(ua) == 6
+end
 
+@testset "Aqua tests - ambiguities" begin
     # See: https://github.com/SciML/OrdinaryDiffEq.jl/issues/1750
     # Test that we're not introducing method ambiguities across deps
     ambs = Aqua.detect_ambiguities(ForwardDiff; recursive = true)
@@ -18,13 +20,8 @@ using Aqua
     @test length(ambs) == 0
 end
 
-@testset "Aqua tests (additional)" begin
-    Aqua.test_undefined_exports(ForwardDiff)
-    Aqua.test_stale_deps(ForwardDiff)
-    Aqua.test_deps_compat(ForwardDiff)
-    Aqua.test_project_extras(ForwardDiff)
-    Aqua.test_project_toml_formatting(ForwardDiff)
-    Aqua.test_piracy(ForwardDiff)
+@testset "Aqua tests - remaining" begin
+    Aqua.test_all(ForwardDiff; ambiguities = false, unbound_args = false)
 end
 
 nothing
