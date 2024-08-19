b7a38f99b85186fa77143871bce1dc15bf521ee0
Kristoffer Carlsson <kcarlsson89@gmail.com>
2024-08-19 18:29:00 +0200

    Stop precomputing chunk sizes into a nonconcrete array (#708)
    This prevents constant propagation of the length of a static array. Alternative to https://github.com/JuliaDiff/ForwardDiff.jl/pull/707
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- test/GradientTest.jl
+++ test/GradientTest.jl
@@ -135,6 +135,9 @@ end
     @test DiffResults.gradient(sresult1) == DiffResults.gradient(result)
     @test DiffResults.gradient(sresult2) == DiffResults.gradient(result)
     @test DiffResults.gradient(sresult3) == DiffResults.gradient(result)
+
+    # make sure this is not a source of type instability
+    @inferred ForwardDiff.GradientConfig(f, sx)
 end
 
 @testset "exponential function at base zero" begin
