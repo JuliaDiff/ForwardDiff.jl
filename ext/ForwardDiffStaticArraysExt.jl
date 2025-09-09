d0d6a0af1060ba80a8dd0b8a6c9cca93ec9fa67d
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2025-09-09 11:03:10 -0300

    improve performance of hessians with static arrays (#751)
    * improve performance of hessians with static arrays
    
    * ForwardDiffStaticArraysExt.jl: import Partials
    
    * Update HessianTest.jl
    
    * Update HessianTest.jl
    
    * Simplify code and fix test
    
    ---------
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- ext/ForwardDiffStaticArraysExt.jl
+++ ext/ForwardDiffStaticArraysExt.jl
@@ -3,7 +3,7 @@ module ForwardDiffStaticArraysExt
 using ForwardDiff, StaticArrays
 using ForwardDiff.LinearAlgebra
 using ForwardDiff.DiffResults
-using ForwardDiff: Dual, partials, GradientConfig, JacobianConfig, HessianConfig, Tag, Chunk,
+using ForwardDiff: Dual, partials, npartials, Partials, GradientConfig, JacobianConfig, HessianConfig, Tag, Chunk,
                    gradient, hessian, jacobian, gradient!, hessian!, jacobian!,
                    extract_gradient!, extract_jacobian!, extract_value!,
                    vector_mode_gradient, vector_mode_gradient!,
@@ -71,8 +71,9 @@ end
 @inline ForwardDiff.jacobian!(result::Union{AbstractArray,DiffResult}, f::F, x::StaticArray, cfg::JacobianConfig) where {F} = jacobian!(result, f, x)
 @inline ForwardDiff.jacobian!(result::Union{AbstractArray,DiffResult}, f::F, x::StaticArray, cfg::JacobianConfig, ::Val) where {F} = jacobian!(result, f, x)
 
-@generated function extract_jacobian(::Type{T}, ydual::StaticArray, x::S) where {T,S<:StaticArray}
-    M, N = length(ydual), length(x)
+@generated function extract_jacobian(::Type{T}, ydual::Union{StaticArray,Partials}, x::S) where {T,S<:StaticArray}
+    M = ydual <: Partials ? npartials(ydual) : length(ydual)
+    N = length(x)
     result = Expr(:tuple, [:(partials(T, ydual[$i], $j)) for i in 1:M, j in 1:N]...)
     return quote
         $(Expr(:meta, :inline))
