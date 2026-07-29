f64c52e71e4b044b5afb5fc4abc10242c7a3088e
David Müller-Widmann <devmotion@users.noreply.github.com>
2026-07-29 10:12:36 +0200

    Only run JET tests on explicitly supported versions

--- test/QATest.jl
+++ test/QATest.jl
@@ -3,14 +3,16 @@ module QATest
 using ForwardDiff
 using Test
 
-using JET: @test_opt
+import JET
 
-@testset "JET" begin
-    # issue #778
-    @test_opt ForwardDiff.derivative(identity, 1.0)
-    @test_opt ForwardDiff.gradient(only, [1.0], ForwardDiff.GradientConfig(only, [1.0], ForwardDiff.Chunk{1}()))
-    @test_opt ForwardDiff.jacobian(identity, [1.0], ForwardDiff.JacobianConfig(identity, [1.0], ForwardDiff.Chunk{1}()))
-    @test_opt ForwardDiff.hessian(only, [1.0], ForwardDiff.HessianConfig(only, [1.0], ForwardDiff.Chunk{1}()))
+if VERSION < v"1.12" || JET.JET_AVAILABLE
+    @testset "JET" begin
+        # issue #778
+        JET.@test_opt ForwardDiff.derivative(identity, 1.0)
+        JET.@test_opt ForwardDiff.gradient(only, [1.0], ForwardDiff.GradientConfig(only, [1.0], ForwardDiff.Chunk{1}()))
+        JET.@test_opt ForwardDiff.jacobian(identity, [1.0], ForwardDiff.JacobianConfig(identity, [1.0], ForwardDiff.Chunk{1}()))
+        JET.@test_opt ForwardDiff.hessian(only, [1.0], ForwardDiff.HessianConfig(only, [1.0], ForwardDiff.Chunk{1}()))
+    end
 end
 
 end # module
